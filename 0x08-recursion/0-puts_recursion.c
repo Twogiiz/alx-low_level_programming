647ebc6ceb761a1de5b26ce7d0c0377af7812052
Twogiiz <goodwillmonyai@gmail.com>
2023-09-03 14:53:34 -0700

    fixed Betty coding style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _puts_recursion - Prints a string followed by a new line using recursion
  * @s: The string to print
@@ -14,10 +13,8 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-
 	/* Print current char */
 	_putchar(*s);
-
 	/* Recursive call */  
 	_puts_recursion(s + 1);
 }
