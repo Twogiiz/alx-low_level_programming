32e05f4a0a8c10680a41d68dd271452acec9470c
Twogiiz <goodwillmonyai@gmail.com>
2023-09-03 14:30:13 -0700

    added _putchar.c file

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+* File: putchar.c
+* Desc: Implements _putchar function to print a character to stdout
+******************************************************************************/
+
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1. 
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
