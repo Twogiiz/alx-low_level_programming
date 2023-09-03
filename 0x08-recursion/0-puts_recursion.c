ccd98b80e4e7a54e64d54af192cb79a2ea2bbaf3
Twogiiz <goodwillmonyai@gmail.com>
2023-09-03 14:40:52 -0700

    Fixed comments in 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,22 @@
-#include "main.h"
-/**
- *_puts_recursion - prints a string, followed by a new line
- *@s: string to print
- */ 
-void _puts_recursion(char *s)
-{ 
-	if (*s == '\0')
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+#include "main.h" 
 
-	else
-		_putchar('\n');
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion
+ * @s: The string to print
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+        /* If string is empty, print new line and return*/
+        if (*s == '\0') {
+                _putchar('\n');
+                return;
+        }
+
+        /* Print current character*/
+        _putchar(*s);
+        
+        /* Recursively call on rest of string*/
+        _puts_recursion(s + 1);
 }
