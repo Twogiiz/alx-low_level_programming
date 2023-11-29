032d950b3f7408bce9cad47c218ac53ba66b8d4e
Twogiiz <Goodwillmonyai@gmail.com>
2023-11-29 15:23:13 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an interger
+ * @array: targets an array holding int
+ * @size: array size
+ * @cmp: a funtion pointer to compare the searched int
+ *
+ * Return: in element match, -1 if the size is <=0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (1);
+	}
+	return (-1);
+}
