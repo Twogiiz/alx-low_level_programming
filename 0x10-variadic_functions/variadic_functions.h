752cbed41f36efc9bb7aa9ec84e42ed8d07c6421
Twogiiz <Goodwillmonyai@gmail.com>
2023-11-30 10:12:33 -0800

    fixed variadic_functions.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC FUNCTIONS_H	
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
